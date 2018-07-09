f2dc9cd2b20be8e5d29c305135c6a80c3dc8edf5
Tahmid Shakil <at.shakil.92@gmail.com>
2018-07-09 10:57:49 +0700

    Redux Integration: Asynchronus Flow Support

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 const enhancers = [];
-const middleware = [];
+const middleware = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
